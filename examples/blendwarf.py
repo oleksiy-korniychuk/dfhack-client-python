552a1e9f6111e544f8a78d7858efc3d62b4dde29
Oleksiy <okorniychuk@proton.me>
2025-07-04 17:38:56 -0500

    Cleaned up the blendwarf.py example to be more clear. Uses one function from CoreProtocal and one from the RemoteFortressReader plugin. Properly imports protobuff types from the _pb2 files.

--- examples/blendwarf.py
+++ examples/blendwarf.py
@@ -1,27 +1,22 @@
 import asyncio
-from dfhack_client_python.dfhack_remote import remote, connect, close, StringMessage, EmptyMessage
+# Import connection management functions
+from dfhack_client_python.dfhack_remote import remote, connect, close
+# Import protobuf message types
+from dfhack_client_python.py_export.CoreProtocol_pb2 import StringMessage
+from dfhack_client_python.py_export.RemoteFortressReader_pb2 import UnitList
 
 ## Declare DFHack exported interfaces
 @remote
-async def GetVersion(output: StringMessage = None): pass 
+async def GetVersion(output: StringMessage = None): pass
 
-from dfhack_client_python.py_export.BasicApi_pb2 import GetWorldInfoOut
-@remote
-async def GetWorldInfo(output: GetWorldInfoOut = None): pass 
-
-from RemoteFortressReader_pb2 import VersionInfo
-@remote(plugin='RemoteFortressReader')
-async def GetVersionInfo(output: VersionInfo = None): pass
-
-from RemoteFortressReader_pb2 import UnitList
 @remote(plugin='RemoteFortressReader')
 async def GetUnitList(output: UnitList = None): pass
 
 # Test
 async def main():
     await connect()
-    print( await GetVersionInfo() )
-    print( "Units: ", len((await GetUnitList()).creature_list) )
+    print( "DFHack Version: ", (await GetVersion()).value )
+    print( "Units: ", len((await GetUnitList()).creature_list))
     await close()
 
 asyncio.run(main())
